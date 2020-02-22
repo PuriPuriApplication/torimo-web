1bbaf27c86283ffdd9ced16e3438282bebd8fb56
a_yoshino <atsuko.yoshino@rakus.co.jp>
2020-02-22 17:34:47 +0900

    add login view

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login">
+        <h2>Welcome Torimo</h2>
+        <img src="/img/twitter.png" @click="submit" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    methods: {
+        submit() {
+            alert('Hello World!!');
+        }
+    }
+};
+</script>
+<style scoped>
+img {
+    width: 200px;
+}
+</style>

4b8ed88b6dd2f122c52c3bf89d5ed97e75a10099
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-02-23 18:30:11 +0900

    add routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () =>
             import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/detail',
+        name: 'Detail',
+        component: () => import('../views/Detail.vue')
     }
 ];
 
